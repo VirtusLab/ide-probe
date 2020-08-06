e8dfdc5811c2228a3462a25ba09a3fe27544e694
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2020-08-06 15:15:38 +0200

    Add couple useful methods for querying modules from project model

--- api/src/main/scala/org/virtuslab/ideprobe/protocol/Project.scala
+++ api/src/main/scala/org/virtuslab/ideprobe/protocol/Project.scala
@@ -6,7 +6,11 @@ case class Project(
     name: String,
     basePath: String,
     modules: Seq[Module]
-)
+) {
+  def moduleNames: Seq[String] = modules.map(_.name)
+  def moduleRefs: Seq[ModuleRef] = modules.map(module => ModuleRef(module.name, ProjectRef(name)))
+  def modulesByNames(names: Set[String]): Seq[Module] = modules.filter(m => names.contains(m.name))
+}
 
 case class Module(name: String,
                   contentRoots: ContentRoots,
