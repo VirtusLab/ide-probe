86f1bd6a25af3073c89c669f04103aa3f00c11c8
Lukasz Wawrzyk <uwawrzyk@twitter.com>
2021-08-11 12:29:34 +0200

    Check setsid just once

--- extensions/bazel/driver/src/main/scala/org/virtuslab/ideprobe/bazel/BazelPluginExtension.scala
+++ extensions/bazel/driver/src/main/scala/org/virtuslab/ideprobe/bazel/BazelPluginExtension.scala
@@ -8,6 +8,15 @@ import org.virtuslab.ideprobe.dependencies.InternalPlugins
 import java.nio.file.Paths
 import scala.language.implicitConversions
 
+object BazelPluginExtension {
+  private lazy val checkSetsid = {
+    // Hack for disabling setsid call with unsupported --wait on older CentOS
+    if (OS.Current == OS.Unix && Shell.run("/usr/bin/setsid", "--wait", "ls").isFailed) {
+      Paths.get(sys.props("user.home"), ".intellij-experiments").write("blaze.command.process.group=false")
+    }
+  }
+}
+
 trait BazelPluginExtension extends BazelOpenProjectFixture with RobotPluginExtension {
   this: IdeProbeFixture =>
 
@@ -19,10 +28,8 @@ trait BazelPluginExtension extends BazelOpenProjectFixture with RobotPluginExten
           s"Current java version: $javaVersion and JAVA_HOME: ${sys.env.get("JAVA_VERSION")}"
       )
     }
-    // Hack for disabling setsid call with unsupported --wait on older CentOS
-    if (OS.Current == OS.Unix && Shell.run("/usr/bin/setsid", "--wait", "ls").isFailed) {
-        Paths.get(sys.props("user.home"), ".intellij-experiments").write("blaze.command.process.group=false")
-      }
+
+    BazelPluginExtension.checkSetsid
   }
   registerFixtureTransformer(InternalPlugins.installCrossVersionPlugin("ideprobe-bazel"))
 
