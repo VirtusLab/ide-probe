d7dd0bd4198cd1474f9e2a1761ff84c0bb61b6fa
Tomasz Pasternak <1066003+tpasternak@users.noreply.github.com>
2022-05-11 11:45:00 +0200

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,24 +15,24 @@ object Dependencies {
     "org.scala-lang" % "scala-library" % version
   )
 
-  val nuProcess = "com.zaxxer" % "nuprocess" % "1.2.6"
+  val nuProcess = "com.zaxxer" % "nuprocess" % "2.0.3"
 
   val remoteRobot = "com.intellij.remoterobot" % "remote-robot" % "0.11.7"
 
   val remoteRobotFixtures = "com.intellij.remoterobot" % "remote-fixtures" % "0.11.13"
 
-  val gson = "com.google.code.gson" % "gson" % "2.8.6"
+  val gson = "com.google.code.gson" % "gson" % "2.9.0"
 
   // because idea plugin packager would only take the root jar which has no classes
   // somehow it fails to see the transitive dependencies (even though the code says it should)
   // so here are all the dependencies explicitly
   val pureConfig = {
-    val typesafeConfig = "com.typesafe" % "config" % "1.4.0"
-    val shapeless = "com.chuusai" %% "shapeless" % "2.3.3"
+    val typesafeConfig = "com.typesafe" % "config" % "1.4.2"
+    val shapeless = "com.chuusai" %% "shapeless" % "2.3.9"
     val pureConfigModules =
       Seq("pureconfig", "pureconfig-macros", "pureconfig-generic", "pureconfig-generic-base", "pureconfig-core")
     pureConfigModules.map { module =>
-      "com.github.pureconfig" %% module % "0.14.0"
+      "com.github.pureconfig" %% module % "0.17.1"
     } ++ Seq(typesafeConfig, shapeless)
   }
 
