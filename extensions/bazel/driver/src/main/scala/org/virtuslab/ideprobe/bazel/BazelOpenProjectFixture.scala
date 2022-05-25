b69522f9f69d7f0f5783a3cc28d2695298095655
Lukasz Wawrzyk <uwawrzyk@twitter.com>
2022-05-25 14:11:05 +0200

    Validate directories before opening bazel project

--- extensions/bazel/driver/src/main/scala/org/virtuslab/ideprobe/bazel/BazelOpenProjectFixture.scala
+++ extensions/bazel/driver/src/main/scala/org/virtuslab/ideprobe/bazel/BazelOpenProjectFixture.scala
@@ -1,5 +1,6 @@
 package org.virtuslab.ideprobe.bazel
 
+import org.virtuslab.ideprobe.Extensions.PathExtension
 import org.virtuslab.ideprobe.protocol.ProjectRef
 import org.virtuslab.ideprobe.robot.RobotPluginExtension
 import org.virtuslab.ideprobe.{RunningIntelliJFixture, WaitLogic}
@@ -11,6 +12,12 @@ trait BazelOpenProjectFixture extends BazeliskExtension { this: RobotPluginExten
       waitLogic: WaitLogic = WaitLogic.Default
   ): ProjectRef = {
     val importSpec = intelliJ.config[BazelImportSpec]("bazel.import")
+
+    importSpec.directories.foreach { dirName =>
+      val dir = intelliJ.workspace.resolve(dirName)
+      assert(dir.isDirectory, s"Not a directory: $dir")
+    }
+
     val robotDriver = intelliJ.probe.withRobot
     BazelProbeDriver(intelliJ.probe)
       .importProject(importSpec, intelliJ.workspace, robotDriver.extendWaitLogic(waitLogic))
