fee1dccdd96bd43e6dd57faa5f206bf4f7de96db
azdrojowa123 <a.zdrojowa@gmail.com>
2022-06-21 14:07:53 +0200

    add possibility to save screenshots without setting env variables

--- core/driver/sources/src/main/scala/org/virtuslab/ideprobe/config/PathsConfig.scala
+++ core/driver/sources/src/main/scala/org/virtuslab/ideprobe/config/PathsConfig.scala
@@ -6,7 +6,7 @@ case class PathsConfig(
     base: Option[Path] = None,
     instances: Option[Path] = None,
     workspaces: Option[Path] = None,
-    screenshots: Option[Path] = sys.env.get("IDEPROBE_SCREENSHOTS_DIR").map(path => Paths.get(path)),
+    screenshots: Option[Path] = None,
     cache: Option[Path] = None,
     trusted: Option[Path] = None
 )
--- core/probePlugin/src/main/scala/org/virtuslab/ideprobe/Screenshot.scala
+++ core/probePlugin/src/main/scala/org/virtuslab/ideprobe/Screenshot.scala
@@ -9,18 +9,15 @@ object Screenshot {
   private val id = new AtomicInteger(0)
 
   private val probeDisplay = sys.env.get("DISPLAY")
-  private val outputDirectory = for {
-    screenshotsDir <- sys.env.get("IDEPROBE_SCREENSHOTS_DIR")
-    testSuite <- sys.env.get("IDEPROBE_TEST_SUITE")
-    testCase <- sys.env.get("IDEPROBE_TEST_CASE")
-    path = Paths.get(screenshotsDir).resolve(testSuite).resolve(testCase)
-  } yield Files.createDirectories(path)
+  private val testSuiteName = getEnvOrDefault("IDEPROBE_TEST_SUITE")
+  private val testCaseName = getEnvOrDefault("IDEPROBE_TEST_CASE")
+  private val screenshotsDir = getEnvOrDefault("IDEPROBE_SCREENSHOTS_DIR")
+  private val outputDirectory = Files.createDirectories(Paths.get(screenshotsDir).resolve(testSuiteName).resolve(testCaseName))
 
   def take(nameSuffix: String = ""): Unit = {
     for {
       display <- probeDisplay
-      directory <- outputDirectory
-      file = directory.resolve(s"${id.incrementAndGet()}$nameSuffix.png")
+      file = outputDirectory.resolve(s"${id.incrementAndGet()}$nameSuffix.png")
     } {
       val process = Runtime.getRuntime.exec(command(display, file))
       if (process.waitFor() != 0) {
@@ -32,5 +29,12 @@ object Screenshot {
 
   private def command(display: String, outputFile: Path): Array[String] = {
     Array("/bin/sh", "-c", s"xwd -display $display -root -silent | convert xwd:- png:${outputFile.toAbsolutePath}")
+  }
+
+  private def getEnvOrDefault(env: String): String = {
+    sys.env.get(env) match {
+      case None => ""
+      case Some(value) => value
+    }
   }
 }
