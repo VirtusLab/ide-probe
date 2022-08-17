157bde3ebb7cad6adc7c5a0943f7839999e97266
LukaszKontowski <49597713+LukaszKontowski@users.noreply.github.com>
2022-08-17 09:51:19 +0200

    added proper message for ProviderNotFoundException (#249)
    * added proper message for ProviderNotFoundException
    
    * replace Try with standard try/catch

--- core/driver/sources/src/main/scala/org/virtuslab/ideprobe/dependencies/Resource.scala
+++ core/driver/sources/src/main/scala/org/virtuslab/ideprobe/dependencies/Resource.scala
@@ -6,6 +6,7 @@ import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.nio.file.ProviderNotFoundException
 import java.util.zip.ZipInputStream
 
 import scala.util.control.NonFatal
@@ -64,8 +65,17 @@ object Resource extends ConfigFormat {
     def installTo(target: Path): Unit
 
     def rootEntries: List[String] = {
-      val fs = FileSystems.newFileSystem(path, this.getClass.getClassLoader)
-      Files.list(fs.getPath("/")).iterator.asScala.map(_.toString).toList
+      try {
+        val fs = FileSystems.newFileSystem(path, this.getClass.getClassLoader)
+        Files.list(fs.getPath("/")).iterator.asScala.map(_.toString).toList
+      } catch {
+        case _: ProviderNotFoundException =>
+          throw new IllegalArgumentException(
+            s"""
+               |The path provided as the plugin path: $path is not valid.
+               |Make sure it points to the plugin file (or .zip archive) and NOT to a directory.""".stripMargin
+          )
+      }
     }
   }
 
