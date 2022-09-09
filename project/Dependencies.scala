a465df836148bbf0b2a8de8f74e76270d107e35d
Scala Steward <scala_steward@virtuslab.com>
2022-09-09 19:53:58 +0000

    Update org.eclipse.jgit to 6.3.0.202209071007-r

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
 
   val nuProcess = "com.zaxxer" % "nuprocess" % "2.0.3"
 
-  val jGit = "org.eclipse.jgit" % "org.eclipse.jgit" % "6.2.0.202206071550-r"
+  val jGit = "org.eclipse.jgit" % "org.eclipse.jgit" % "6.3.0.202209071007-r"
 
   val remoteRobot = "com.intellij.remoterobot" % "remote-robot" % "0.11.15"
 
