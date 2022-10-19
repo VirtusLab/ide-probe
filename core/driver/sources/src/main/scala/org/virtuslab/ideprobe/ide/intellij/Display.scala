ad80031e7d955ff85e41b7ca7e855cfc8acabb4e
LukaszKontowski <49597713+LukaszKontowski@users.noreply.github.com>
2022-10-19 09:03:49 +0200

    Add config for display mode (native/xvfb) (#272)
    * add config for display mode (native/xvfb)
    
    * improve the new display config key
    
    * remove unit tests for the display mode
    
    * Replace Display.Mode with InstalledIntelliJ.displayMode to avoid multiple method calls

--- core/driver/sources/src/main/scala/org/virtuslab/ideprobe/ide/intellij/Display.scala
+++ core/driver/sources/src/main/scala/org/virtuslab/ideprobe/ide/intellij/Display.scala
@@ -2,19 +2,10 @@ package org.virtuslab.ideprobe.ide.intellij
 
 sealed trait Display
 object Display {
-  private val Default = Native
-
-  val Mode: Display = {
-    Option(System.getenv("IDEPROBE_DISPLAY"))
-      .map(fromName)
-      .getOrElse(Default)
-  }
 
   val XvfbDisplayId = "7" // doesn't really matter as long as it is not 0
 
-  println(s"Display mode: $Mode")
-
-  private def fromName(name: String): Display = {
+  def fromName(name: String): Display = {
     name.toLowerCase() match {
       case "xvfb"   => Xvfb
       case "native" => Native
