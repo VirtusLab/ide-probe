3cdb91e537036231ac62512c3b21a5a0acd8641d
LukaszKontowski <49597713+LukaszKontowski@users.noreply.github.com>
2022-10-19 13:34:41 +0200

    Allow for globs in `probe.resolvers.intellij.repositories` config  (#285)
    * Allow for globs in probe.resolvers.intellij.repositories config
    
    * Fix globes logic and allow for multiple globes in one pattern
    
    * Add example with description in reference.conf
    
    * Fix misspell in reference.conf description
    
    Co-authored-by: Paweł Lipski <pawel.p.lipski@gmail.com>
    
    * PR fixes - fix resolving logic and improve code style
    
    * Fi logic for checking las part of the candidate path
    
    * fix logic for non-local patterns
    
    * Improve code style - use String#split
    
    * Revert "Improve code style - use String#split"
    
    This reverts commit c6c43a4a844199599b9965a603ffa48d3b9f6f44.
    
    * Improve test scanarios
    
    Co-authored-by: Paweł Lipski <pawel.p.lipski@gmail.com>

--- core/driver/sources/src/main/scala/org/virtuslab/ideprobe/dependencies/IntelliJResolver.scala
+++ core/driver/sources/src/main/scala/org/virtuslab/ideprobe/dependencies/IntelliJResolver.scala
@@ -1,7 +1,13 @@
 package org.virtuslab.ideprobe.dependencies
 
+import java.nio.file.Paths
+
+import scala.annotation.tailrec
+
 import org.virtuslab.ideprobe.IntelliJFixture
 import org.virtuslab.ideprobe.config.DependenciesConfig
+
+import org.virtuslab.ideprobe.Extensions.PathExtension
 
 trait IntelliJResolver {
   def community: DependencyResolver[IntelliJVersion]
@@ -37,9 +43,54 @@ case class IntelliJPatternResolver(pattern: String) extends IntelliJResolver {
       "version" -> version.releaseOrBuild,
       "release" -> version.release.getOrElse("snapshot-release")
     )
-    val replaced = replacements.foldLeft(pattern) { case (path, (pattern, replacement)) =>
+    val replacedBeforeResolvingGlobs = replacements.foldLeft(pattern) { case (path, (pattern, replacement)) =>
       path.replace(s"[$pattern]", replacement)
     }
+    val replaced =
+      if (replacedBeforeResolvingGlobs.startsWith("file:"))
+        resolveGlobsInPattern(replacedBeforeResolvingGlobs)
+      else
+        replacedBeforeResolvingGlobs
+
     Dependency(replaced)
+  }
+
+  private def resolveGlobsInPattern(pathPattern: String): String =
+    replaceGlobsWithExistingDirectories(List(pathPattern), pathPattern).head
+
+  // Solution below assumes that each * character is used to mark one part of the path (one atomic directory),
+  // for example: "file:///home/.cache/ides/com.jetbrains.intellij.idea/ideaIC/[revision]/*/ideaIC-[revision]/".
+  // Wildcard character should NOT be used as the last element of the pattern - to avoid ambiguous results.
+  // Works only for files in local filesystem.
+  @tailrec
+  private def replaceGlobsWithExistingDirectories(paths: List[String], originalPattern: String): List[String] =
+    if (paths.exists(pathMightBeValidResource(_, originalPattern)))
+      paths.filter(pathMightBeValidResource(_, originalPattern))
+    else
+      replaceGlobsWithExistingDirectories(paths.flatMap(replaceFirstFoundWildcardWithDirectories), originalPattern)
+
+  private def pathMightBeValidResource(candidatePath: String, originalPattern: String): Boolean =
+    !candidatePath.contains('*') && // below - make sure that candidatePath's last path element is same as in pattern
+      candidatePath.substring(candidatePath.lastIndexOf('/')) == originalPattern.substring(
+        originalPattern.lastIndexOf('/')
+      ) && Paths.get(candidatePath.replace("file:", "")).exists
+
+  private def replaceFirstFoundWildcardWithDirectories(path: String): List[String] = {
+    def removeLastFileSeparator(path: String): String = if (path.endsWith("/")) path.init else path
+
+    val pathUntilWildcard = Paths.get(path.substring(0, path.indexOf('*')).replace("file:", ""))
+    val stringPathAfterWildcard = path.substring(path.indexOf('*') + 1)
+
+    if (pathUntilWildcard.exists) {
+      pathUntilWildcard
+        .directChildren()
+        .map { replaced =>
+          (if (path.startsWith("file:")) "file:" else "") + removeLastFileSeparator(
+            replaced.toString
+          ) + stringPathAfterWildcard
+        }
+    } else {
+      Nil
+    }
   }
 }
