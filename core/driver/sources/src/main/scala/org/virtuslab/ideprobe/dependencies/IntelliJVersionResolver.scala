cf7346491cb9ae7248677e2c78499a537f2d4e58
LukaszKontowski <49597713+LukaszKontowski@users.noreply.github.com>
2022-10-29 17:09:32 +0200

    Fix IntelliJVersionResolver.version logic for macOs (#303)

--- core/driver/sources/src/main/scala/org/virtuslab/ideprobe/dependencies/IntelliJVersionResolver.scala
+++ core/driver/sources/src/main/scala/org/virtuslab/ideprobe/dependencies/IntelliJVersionResolver.scala
@@ -5,10 +5,15 @@ import java.nio.file.Path
 import com.google.gson.JsonParser
 
 import org.virtuslab.ideprobe.Extensions._
+import org.virtuslab.ideprobe.OS
 
 object IntelliJVersionResolver {
   def version(intelliJPath: Path): IntelliJVersion = {
-    val productInfo = intelliJPath.resolve("product-info.json").content()
+    val productInfo =
+      if (OS.Current == OS.Mac && intelliJPath.name == "Contents")
+        intelliJPath.resolve("Resources").resolve("product-info.json").content()
+      else
+        intelliJPath.resolve("product-info.json").content()
     val productInfoJsonObject = JsonParser.parseString(productInfo).getAsJsonObject
     val version = productInfoJsonObject.getAsJsonPrimitive("version").getAsString
     val buildNumber = productInfoJsonObject.getAsJsonPrimitive("buildNumber").getAsString
