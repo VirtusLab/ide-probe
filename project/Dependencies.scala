14212099ec67911f99048041abed7f94eb2ff42b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-07-11 10:23:00 +0200

    Update commons-io to 2.15.1 (#403)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   )
 
   val commonsCodec = "commons-codec" % "commons-codec" % "1.16.1"
-  val commonsIO = "commons-io" % "commons-io" % "2.15.0"
+  val commonsIO = "commons-io" % "commons-io" % "2.15.1"
 
   val nuProcess = "com.zaxxer" % "nuprocess" % "2.0.6"
 
