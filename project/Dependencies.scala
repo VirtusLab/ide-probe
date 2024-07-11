34bdab493ab3178d3e3b3ab2df132e5339c4e240
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-07-11 10:23:09 +0200

    Update gson to 2.11.0 (#431)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
 
   val remoteRobotFixtures = "com.intellij.remoterobot" % "remote-fixtures" % "0.11.22"
 
-  val gson = "com.google.code.gson" % "gson" % "2.10.1"
+  val gson = "com.google.code.gson" % "gson" % "2.11.0"
 
   val jsoup = "org.jsoup" % "jsoup" % "1.18.1"
 
